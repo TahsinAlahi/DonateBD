5fc2d21726d493fab6cac15b0529d142cf2c1f8f
Tahsin <alahitahsin@gmail.com>
2024-09-24 23:27:58 +0600

    added page toggle

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 let balanceEl = document.getElementById("balance");
 const donationBtns = document.getElementsByClassName("donateBtn");
 const historyPage = document.getElementById("historyPage");
+const donationPage = document.getElementById("donationPage");
 let history = [];
 
 const donatePageBtn = document.getElementById("donatePageBtn");
 const historyPageBtn = document.getElementById("historyPageBtn");
+
+function handlePageToggle(e) {
+  if (
+    donatePageBtn.classList.contains("active") &&
+    e.target.id === "historyPageBtn"
+  ) {
+    donatePageBtn.classList.replace("active", "inactive");
+    historyPageBtn.classList.replace("inactive", "active");
+  } else if (
+    historyPageBtn.classList.contains("active") &&
+    e.target.id === "donatePageBtn"
+  ) {
+    historyPageBtn.classList.replace("active", "inactive");
+    donatePageBtn.classList.replace("inactive", "active");
+  }
+
+  if (donatePageBtn.classList.contains("active")) {
+    donationPage.classList.remove("hidden");
+    historyPage.classList.add("hidden");
+  } else {
+    donationPage.classList.add("hidden");
+    historyPage.classList.remove("hidden");
+  }
+}
 
 function historyLoad(donateAmount, title) {
   const newData = {
@@ -53,5 +78,5 @@ function addHistoryItem(historyObj) {
   historyPage.appendChild(div);
 }
 
-// donatePageBtn.addEventListener("click", handlePageToggle);
-// historyPageBtn.addEventListener("click", handlePageToggle);
+donatePageBtn.addEventListener("click", handlePageToggle);
+historyPageBtn.addEventListener("click", handlePageToggle);
