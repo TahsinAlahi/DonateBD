69df85fac49baf55ae5927b1cf612d5ec6476028
Tahsin <alahitahsin@gmail.com>
2024-09-24 20:56:46 +0600

    added donate function

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const balance = document.getElementById("balance");
-const donationBtn = document.getElementsByClassName("donateBtn");
+let balanceEl = document.getElementById("balance");
+let donationBtns = document.getElementsByClassName("donateBtn");
+let history = {};
+
+function handleDonate(e) {
+  let card = e.target.parentNode;
+  let totalDonatedEl = card.querySelector("span");
+  let donateAmountEl = card.querySelector("input");
+
+  let title = card.querySelector("h2").innerText;
+  let totalDonated = parseInt(totalDonatedEl.innerText);
+  let donateAmount = parseInt(donateAmountEl.value);
+  let balance = parseInt(balanceEl.innerText);
+
+  if (balance - donateAmount <= 0) return;
+
+  balanceEl.innerText = balance - donateAmount;
+  totalDonatedEl.innerText = totalDonated + donateAmount;
+  // donateAmountEl.value = "";
+}
+
+for (let donateBtn of donationBtns) {
+  donateBtn.addEventListener("click", handleDonate);
+  // donateBtn.click();
+}
